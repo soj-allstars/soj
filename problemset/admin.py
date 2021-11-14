39c0bff3daebb72483e6b5d129631cb392f7bd81
jp <abzmglqyp@gmail.com>
2021-11-14 18:29:36 +0800

    feat: add ability to change test cases in admin

--- problemset/admin.py
+++ problemset/admin.py
@@ -1,10 +1,28 @@
+from django import forms
 from django.contrib import admin
 from django.contrib import messages
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from problemset.models import Problem, Solution
+from problemset.views import save_input_files
 from judge.tasks import send_check_request
 
+import json
+import os
 import shutil
+
+
+def get_problem_test_cases_as_json(problem_id: int):
+    test_cases = []
+    idx = 1
+    while True:
+        input_file = f'{settings.PROBLEM_DATA_DIR}/{problem_id}/{idx}.in'
+        if not os.path.isfile(input_file):
+            break
+        with open(input_file, 'r') as f:
+            test_cases.append(f.read())
+        idx += 1
+    return json.dumps(test_cases)
 
 
 class SolutionInline(admin.StackedInline):
@@ -12,11 +30,45 @@ class SolutionInline(admin.StackedInline):
     extra = 0
 
 
+class ProblemForm(forms.ModelForm):
+    test_cases = forms.CharField(required=True, widget=forms.Textarea)
+
+    def clean_test_cases(self):
+        test_cases = self.cleaned_data['test_cases']
+        try:
+            obj = json.loads(test_cases)
+        except json.JSONDecodeError:
+            raise ValidationError("Not a valid JSON value!")
+        if type(obj) is not list:
+            raise ValidationError("Not a valid JSON list!")
+        for ele in obj:
+            if type(ele) is not str:
+                raise ValidationError("Every element should be a string!")
+        return obj
+
+
 class ProblemAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'checker_type', 'visible')
     inlines = [SolutionInline]
     list_filter = ('visible',)
     actions = ['write_inputs_answers', 'mark_as_visible', 'mark_as_invisible']
+    form = ProblemForm
+    fields = (
+        'id', 'title', 'visible', 'description', 'time_limit', 'memory_limit', 'sample_inputs',
+        'sample_outputs', 'note', 'checker_type', 'checker_code', 'test_cases',
+    )
+    readonly_fields = ('id',)
+
+    def get_form(self, request, obj, change, **kwargs):
+        form = super().get_form(request, obj=obj, change=change, **kwargs)
+        if obj:
+            form.base_fields['test_cases'].initial = get_problem_test_cases_as_json(obj.id)
+        return form
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        test_cases = form.cleaned_data['test_cases']
+        save_input_files(obj.id, test_cases)
 
     def write_inputs_answers(self, request, problems):
         ok = True
